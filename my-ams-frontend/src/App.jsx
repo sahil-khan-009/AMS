19bb5bb539fea142c543cb74b37a87649f3cfc58
sahil khan <khanshil504@gmail.com>
2025-02-28 15:34:03 +0530

    mmm

--- my-ams-frontend/src/App.jsx
+++ my-ams-frontend/src/App.jsx
@@ -1,18 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./Component/Layout.jsx";
-import Login from "./Pages/Login.jsx";
-import Register from "./Pages/Register.jsx";
-import UserDashboard from "./Pages/UserDashboard.jsx";
-import Appointment from "./Pages/Appointment.jsx";
-import AppointmentStatus from "./Pages/AppointmentStatus.jsx";
-import Profile from "./Pages/Profile.jsx";
-import UpdateDetails from "./Pages/UpdateDetails.jsx";
-import Dashboard from "./Pages/Dashboard.jsx";
-import Notification from "./Pages/Notification.jsx";
-import { AppointmentProvider } from "./context/AppointmentContext.jsx";
-import AdminSideBar from "./Admin/Component/AdminSideBar.jsx";
-import AdminDashboard from "./Admin/Page/AdminDashboard.jsx";
-import AdminDepartment from "./Admin/Page/AdminDepartment.jsx";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './Component/Layout.jsx';
+import Login from './Pages/Login.jsx';
+import Register from './Pages/Register.jsx';
+import UserDashboard from './Pages/UserDashboard.jsx';
+import Appointment from './Pages/Appointment.jsx';
+import AppointmentStatus from './Pages/AppointmentStatus.jsx';
+import Profile from './Pages/Profile.jsx';
+import UpdateDetails from './Pages/UpdateDetails.jsx';
+import Dashboard from './Pages/Dashboard.jsx';
+import Notification from './Pages/Notification.jsx';
+import { AppointmentProvider } from './context/AppointmentContext.jsx';
+import AdminSideBar from './Admin/Component/AdminSideBar.jsx';
+import AdminDashboard from './Admin/Page/AdminDashboard.jsx';
+import AdminDepartment from '../src/Admin/Page/AdminDepartment.jsx';
+import AddDepartment from './Admin/Page/AddDepartment.jsx';
+import AddDoctor from './Admin/Page/AddDoctor.jsx';
 
 function App() {
   return (
@@ -23,7 +25,7 @@ function App() {
           <Route path="register" element={<Register />} />
           <Route path="/login" element={<Login />} />
 
-          <Route path="/UserDashboard" element={<UserDashboard />}>
+          <Route path='/UserDashboard' element={<UserDashboard />}>
             <Route index element={<Dashboard />} />
             <Route path="Appointment" element={<Appointment />} />
             <Route path="AppointmentStatus" element={<AppointmentStatus />} />
@@ -32,10 +34,12 @@ function App() {
             <Route path="Notification" element={<Notification />} />
           </Route>
 
-          <Route path="/AdminDashboard" element={<AdminDashboard />}/>
-            <Route path="/AdminSideBar" element={<AdminSideBar />} />
-            <Route path="/AdminDepartment" element={<AdminDepartment />} />
-         
+          <Route path='/AdminSideBar' element={<AdminSideBar />} >
+            <Route index element={<AdminDashboard />} />
+            <Route path='AdminDepartment' element={<AdminDepartment/>}/>
+            <Route path='AddDepartment' element={<AddDepartment/>}/>
+            <Route path='AddDoctor' element={<AddDoctor/>}/>
+          </Route>
         </Routes>
       </Router>
     </AppointmentProvider>
