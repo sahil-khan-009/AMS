065ca3cda26c0441450a9663cd743215de936777
sahil khan <khanshil504@gmail.com>
2025-04-09 15:01:51 +0530

    changes by Sahil

--- my-ams-frontend/src/Admin/Page/UserAppointment.jsx
+++ my-ams-frontend/src/Admin/Page/UserAppointment.jsx
@@ -1,168 +1,243 @@
-import React, { useState } from 'react';
-import Adminnav from '../Component/Adminnav';
+import React, { useEffect, useState } from "react";
+import Adminnav from "../Component/Adminnav";
 import { FaCheck, FaFilter } from "react-icons/fa";
 import { ImCancelCircle } from "react-icons/im";
 import { IoIosArrowBack } from "react-icons/io";
 import { IoIosArrowForward } from "react-icons/io";
 import { LuView } from "react-icons/lu";
-import '../PageStyle/UserAppointment.css'
-
-
-
+import "../PageStyle/UserAppointment.css";
+import { adminApi } from "../../Api-folder/Api";
 
 const UserAppointment = () => {
-    const [filter, setFilter] = useState("All");
-    const [search, setSearch] = useState("");
-    const [selectedDate, setSelectedDate] = useState(""); // New state for date filter
-    const [currentPage, setCurrentPage] = useState(1);
-    const appointmentsPerPage = 10;
+  const [appointment, Setappointment] = useState([]);
+  const [filter, setFilter] = useState("All");
+  const [search, setSearch] = useState("");
+  const [selectedDate, setSelectedDate] = useState(""); // New state for date filter
+  const [currentPage, setCurrentPage] = useState(1);
+  const appointmentsPerPage = 10;
 
-    const appointments = [
-        { id: 1, name: "Mia Nelson", email: "mia@example.com", doctor: "Dr. Smith", department: "Cardiology", date: "2024-03-10", status: "Pending" },
-        { id: 2, name: "John Doe", email: "john@example.com", doctor: "Dr. Brown", department: "Neurology", date: "2024-03-10", status: "Approved" },
-        { id: 3, name: "John Doe", email: "john@example.com", doctor: "Dr. Brown", department: "Neurology", date: "2024-03-10", status: "Approved" },
-        { id: 4, name: "John Doe", email: "john@example.com", doctor: "Dr. Brown", department: "Neurology", date: "2024-03-10", status: "Approved" },
-        { id: 5, name: "John Doe", email: "john@example.com", doctor: "Dr. Brown", department: "Neurology", date: "2024-03-10", status: "Approved" },
-        { id: 6, name: "John Doe", email: "john@example.com", doctor: "Dr. Brown", department: "Neurology", date: "2024-03-10", status: "Approved" },
-        { id: 7, name: "John Doe", email: "john@example.com", doctor: "Dr. Brown", department: "Neurology", date: "2024-03-10", status: "Approved" },
-        { id: 8, name: "John Doe", email: "john@example.com", doctor: "Dr. Brown", department: "Neurology", date: "2024-03-10", status: "Approved" },
-        { id: 9, name: "John Doe", email: "john@example.com", doctor: "Dr. Brown", department: "Neurology", date: "2024-03-10", status: "Approved" },
-        { id: 10, name: "John Doe", email: "john@example.com", doctor: "Dr. Brown", department: "Neurology", date: "2024-03-10", status: "Approved" },
-        { id: 11, name: "John Doe", email: "john@example.com", doctor: "Dr. Brown", department: "Neurology", date: "2024-03-10", status: "Approved" },
-    ];
+  // Getting all appointment
 
-    const filteredAppointments = appointments.filter(appt => {
-        const matchesSearch = search === "" || appt.name.toLowerCase().includes(search.toLowerCase()) ||
-            appt.doctor.toLowerCase().includes(search.toLowerCase()) ||
-            appt.department.toLowerCase().includes(search.toLowerCase());
+  const fetchAppointment = async () => {
+    try {
+      const response = await adminApi.totalAppointment();
+      console.log("this is total appointment response ", response.data);
+      Setappointment(response.data);
+    } catch (err) {
+      console.log("this is catch error", err.message);
+    }
+  };
 
-        const matchesFilter = filter === "All" || appt.status === filter;
+  useEffect(() => {
+    fetchAppointment();
+  }, []);
 
-        const matchesDate = selectedDate === "" || appt.date === selectedDate; // Date filtering
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [filter, search, selectedDate]);
 
-        return matchesSearch && matchesFilter && matchesDate;
-    });
+  const filteredAppointments = appointment.filter((appt) => {
+    const matchesSearch =
+      search === "" ||
+      appt.patientName.toLowerCase().includes(search.toLowerCase()) ||
+      appt.doctorName.toLowerCase().includes(search.toLowerCase()) ||
+      appt.department.toLowerCase().includes(search.toLowerCase());
 
-    const indexOfLastAppointment = currentPage * appointmentsPerPage;
-    const indexOfFirstAppointment = indexOfLastAppointment - appointmentsPerPage;
-    const currentAppointments = filteredAppointments.slice(indexOfFirstAppointment, indexOfLastAppointment);
-    const totalPages = Math.ceil(filteredAppointments.length / appointmentsPerPage);
+    const matchesFilter = filter === "All" || appt.appointmentStatus === filter;
 
-    return (
-        <div className='full-height-bg'style={{paddingTop:'5em'}}>
-            <Adminnav />
-            <h4 >Appointments</h4>
-            <hr />
+    const matchesDate =
+      selectedDate === "" || appt.appointmentDate === selectedDate; // Date filtering
 
-            <div className="d-flex flex-wrap items-center gap-3">
-                <div className='status'>
-                    <label className='me-2'>Status:</label>
-                    <select
-                        className="border px-3 py-2 rounded"
-                        value={filter}
-                        onChange={(e) => {
-                            if (e.target.value === "All") {
-                                setSearch("");
-                                setSelectedDate("");
-                            }
-                            setFilter(e.target.value);
-                        }}
-                    >
-                        <option value="All">All</option>
-                        <option value="Pending">Pending</option>
-                        <option value="Approved">Approved</option>
-                    </select>
+    return matchesSearch && matchesFilter && matchesDate;
+  });
 
-                </div>
-                <div className='byDate'>
-                    <label className='me-2'>By Date:</label>
-                    <input
-                        type="date"
-                        className=" byDate border px-3 py-2 rounded"
-                        value={selectedDate}
-                        onChange={(e) => setSelectedDate(e.target.value)}
-                    />
-                </div>
-                <div className='bySearch'>
-                    <label className='me-2'>By Search:</label>
-                    <input
-                        type="text"
-                        placeholder="Search by Name, Doctor, or Department..."
-                        className="bySearch border px-3 py-2 rounded"
-                        value={search}
-                        onChange={(e) => setSearch(e.target.value)}
-                    />
-                </div>
-                
-            </div>
-            <div className='table-responsive mt-2'>
-                <table className="table table-bordered text-center" >
-                    <thead className="thead">
-                        <tr>
-                            <th scope="col">Sr. No</th>
-                            <th scope="col">Patient Name</th>
-                            <th scope="col">Patient Email</th>
-                            <th scope="col">Doctor Name</th>
-                            <th scope="col">Department</th>
-                            <th scope="col">Appointment Date</th>
-                            <th scope="col">Status</th>
-                            <th scope="col">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {currentAppointments.map((appt, index) => (
-                            <tr key={appt.id}>
-                                <td >{indexOfFirstAppointment + index + 1}</td>
-                                <td >{appt.name}</td>
-                                <td >{appt.email}</td>
-                                <td >{appt.doctor}</td>
-                                <td>{appt.department}</td>
-                                <td >{appt.date}</td>
-                                <td className={`${appt.status === "Pending" ? "text-warning" : "text-success"}`}>{appt.status}</td>
-                                <td>
-                                    {appt.status === "Pending" ? (
-                                        <div>
-                                            <i className="me-3 text-success " type="button"><FaCheck size={20} /></i>
-                                            <i className="text-danger " type="button"><ImCancelCircle size={20} /></i>
-                                        </div>
-                                    ) : (
-                                        <i className="text-dark" type="button"><LuView size={20} /></i>
-                                    )}
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-            <div className="d-flex justify-content-center ">
-                <div className="d-flex align-items-center gap-2">
-                    <button
-                        className="btn btn-secondary px-2 py-1 rounded-circle"
-                        onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))}
-                        disabled={currentPage === 1}
-                    >
-                        <IoIosArrowBack className="text-white" />
-                    </button>
-                    {Array.from({ length: totalPages }, (_, i) => (
-                        <button
-                            key={i + 1}
-                            className={`btn px-3 py-2 rounded-circle ${currentPage === i + 1 ? "btn-primary" : "btn-secondary"}`}
-                            onClick={() => setCurrentPage(i + 1)}
-                        >
-                            {i + 1}
-                        </button>
-                    ))}
-                    <button
-                        className="btn btn-secondary px-2 py-1 rounded-circle"
-                        onClick={() => setCurrentPage(prev => Math.min(prev + 1, totalPages))}
-                        disabled={currentPage === totalPages}
-                    >
-                        <IoIosArrowForward className="text-white" />
-                    </button>
-                </div>
-            </div>
+  const indexOfLastAppointment = currentPage * appointmentsPerPage;
+  const indexOfFirstAppointment = indexOfLastAppointment - appointmentsPerPage;
+  const currentAppointments = filteredAppointments.slice(
+    indexOfFirstAppointment,
+    indexOfLastAppointment
+  );
+  const totalPages = Math.ceil(
+    filteredAppointments.length / appointmentsPerPage
+  );
+
+  // Bussiness logic of apporoved appointment
+
+  const approvedAppointments = async (id, status) => {
+    try {
+      const response = await adminApi.approvedAppointment(id, status);
+      console.log("Appointment Status Updated:", response.data);
+      if(response.data.message){
+        window.location.reload();
+      }
+    } catch (err) {
+      console.error("Error updating appointment status:", err.message);
+    }
+  };
+
+  // bussiness logic of cancel appointment
+  const cancelledAppointment = async (id,status)=>{
+    try{
+      const response = await adminApi.cancelAppointment(id,status);
+      console.log("Appointment status updated : ",response.data);
+      if(response.data.message){
+        window.location.reload();
+      }
+
+    }catch(err){
+      console.error("error updating appointment status",err.message);
+
+    }
+
+  }
+  
+  return (
+    <div className="full-height-bg" style={{ paddingTop: "5em" }}>
+      <Adminnav />
+      <h4>Appointments</h4>
+      <hr />
+
+      <div className="d-flex flex-wrap items-center gap-3">
+        <div className="status">
+          <label className="me-2">Status:</label>
+          <select
+            className="border px-3 py-2 rounded"
+            value={filter}
+            onChange={(e) => {
+              if (e.target.value === "All") {
+                setSearch("");
+                setSelectedDate("");
+              }
+              setFilter(e.target.value);
+            }}
+          >
+            <option value="All">All</option>
+            <option value="pending">Pending</option>
+            <option value="confirmed">Confirmed</option>
+          </select>
         </div>
-    );
+        <div className="byDate">
+          <label className="me-2">By Date:</label>
+          <input
+            type="date"
+            className=" byDate border px-3 py-2 rounded"
+            value={selectedDate}
+            onChange={(e) => setSelectedDate(e.target.value)}
+          />
+        </div>
+        <div className="bySearch">
+          <label className="me-2">By Search:</label>
+          <input
+            type="text"
+            placeholder="Search by Name, Doctor, or Department..."
+            className="bySearch border px-3 py-2 rounded"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="table-responsive mt-2">
+        <table className="table table-bordered text-center">
+          <thead className="thead">
+            <tr>
+              <th scope="col">Sr. No</th>
+              <th scope="col">Patient Name </th>
+              <th scope="col">Patient Email</th>
+              <th scope="col">Doctor Name</th>
+              <th scope="col">Department</th>
+              <th scope="col">Appointment Date</th>
+              <th scope="col">Status</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {currentAppointments.map((appt, index) => (
+              <tr key={appt._id}>
+                <td>{indexOfFirstAppointment + index + 1}</td>
+                <td>{appt.patientName}</td>
+                <td>{appt.patientemail}</td>
+                <td>{appt.doctorName}</td>
+                <td>{appt.department}</td>
+                <td>
+                  {" "}
+                  {new Date(appt.appointmentDate).toLocaleDateString("en-GB", {
+                    day: "2-digit",
+                    month: "2-digit",
+                    year: "numeric",
+                  })}
+                </td>
+                <td
+                  className={`${
+                    appt.appointmentStatus === "pending"
+                      ? "text-warning"
+                      : "text-success"
+                  }`}
+                >
+                  {appt.appointmentStatus}
+                </td>
+                <td>
+                  {appt.appointmentStatus === "pending" ? (
+                    <div>
+                      <i
+                        className="me-3 text-success "
+                        type="button"
+                        onClick={() => approvedAppointments(appt._id, "confirm")}
+                      >
+                        <FaCheck size={20} /> {/*this is tick ✅ */}
+                      </i>
+                      <i className="text-danger " type="button" onClick={()=> cancelledAppointment(appt._id,"cancel")}>
+                        <ImCancelCircle size={20} /> {/* this is ❎ */}
+                      </i>
+                    </div>
+                  ) : (
+                    <i className="text-dark" type="button"> 
+                      <LuView size={20} /> 
+                    </i>
+                  )}
+                </td>
+              </tr>
+            ))} 
+          </tbody>
+        </table>
+      </div>
+      <div className="d-flex justify-content-center ">
+        <div className="d-flex align-items-center gap-2">
+          <button
+            className="btn btn-secondary px-2 py-1 rounded-circle"
+            onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+            disabled={currentPage === 1}
+          >
+            <IoIosArrowBack className="text-white" />
+          </button>
+          {Array.from({ length: totalPages }, (_, i) => (
+            <button
+              key={i + 1}
+              className={`btn px-3 py-2 rounded-circle ${
+                currentPage === i + 1 ? "btn-primary" : "btn-secondary"
+              }`}
+              onClick={() => setCurrentPage(i + 1)}
+            >
+              {i + 1}
+            </button>
+          ))}
+          <button
+            className="btn btn-secondary px-2 py-1 rounded-circle"
+            onClick={() =>
+              setCurrentPage((prev) => Math.min(prev + 1, totalPages))
+            }
+            disabled={currentPage === totalPages}
+          >
+            <IoIosArrowForward className="text-white" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserAppointment;
+
+// console.log("status-------",status);
+// console.log("id------",id);
+// const Id = id;
+// const confirm = status;
--- my-ams-frontend/src/Pages/Appointment.jsx
+++ my-ams-frontend/src/Pages/Appointment.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import "../PagesStyles/Appointment.css";
 import { useAppointment } from "../context/AppointmentContext";
-import { apiService } from "../Api-folder/Api";
+import { apiService ,adminApi} from "../Api-folder/Api";
 import DashboardNav from "../Component/DashboardNav";
+import { useStripe, useElements, CardElement } from "@stripe/react-stripe-js";
+
 
 function Appointment() {
   const [dropDownValue, setDropDownValue] = useState([]);
@@ -11,7 +13,13 @@ function Appointment() {
   const [date, setDate] = useState("");
   const [description, setDescription] = useState("");
   const [message, Setmessage] = useState("");
+  const [departmentPay, SetdepartmentPay] = useState({
+    name: "react for fb",
+    price: 10,
+    product: " HOSPITAL",
+  }); // New state for department
 
+  
   const {
     selectedDepartment,
     setSelectedDepartment,
@@ -66,10 +74,17 @@ function Appointment() {
     return () => clearTimeout(timer);
   }, [message]);
 
+
+  // Payment=======================
+  
+  
+
   return (
-    <div className="full-height-bg" style={{paddingTop:'5em'}}>
+    <div className="full-height-bg" style={{ paddingTop: "5em" }}>
       <DashboardNav />
       <h3>Make Appointment</h3>
+      
+
       <hr />
       {message && (
         <div
@@ -231,217 +246,3 @@ function Appointment() {
 }
 
 export default Appointment;
-
-// import React, { useEffect, useState } from "react";
-// import "../PagesStyles/Appointment.css";
-// import axios from "axios";
-
-// function Appointment() {
-//   const [dropDownValue, SetdropDownValue] = useState([]);
-//   const [selectedDepartment, setSelectedDepartment] = useState("");
-//   const [selectedAvailibilit, setselectedAvailibilit] = useState("");
-//   const [availibility,setAvailibility]=useState("");
-//   const [patientName, SetPatientName] = useState("");
-//   const [email, Setemail] = useState("");
-//   const [date,Setdate] = useState("");
-//   const [description,Setdescryption] = useState("");
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await axios.get(
-//           "http://localhost:4000/api/doctor/Department",
-//           {
-//             withCredentials: true,
-//           }
-//         );
-
-//         SetdropDownValue(response);
-
-//         console.log("DropDownvalues===========", dropDownValue);
-//         // SetdoctorName(doctorname)
-//         console.log("response---------------", response);
-//       } catch (err) {
-//         console.log("it is a catch error:", err.message);
-//       }
-//     };
-
-//     fetchData(); // Call the async function
-//   }, []);
-
-//   const handleSubmit = async (e)=>{
-//     e.preventDefault();
-//     const fromData = {
-//             department :selectedDepartment,
-//             doctorName: selectedAvailibilit,
-//             availability:availibility,
-//             patientName:patientName,
-//             patientemail:email,
-//             appointmentDate:date,
-//             description:description
-//   };
-
-//   try{
-//     const response = await axios.post("")
-
-//   }catch(err){
-
-//   }
-// }
-
-//   return (
-//     <div className=" col-md-10 col-lg-12 dashboard-content">
-//       <nav className="navbar-expand-lg mb-5">
-//         <a className="brand">Add Appointment</a>
-//       </nav>
-//       <div className="form-container mx-auto">
-//         <form onSubmit={handleSubmit}>
-//           <div className="row g-3 mb-3">
-//             <div className="col-6 col-md-12">
-//               <label htmlFor="department" className="form-label">
-//                 Department:
-//               </label>
-//               <select
-//                 className="form-select"
-//                 id="department"
-//                 required
-//                 onChange={(e) => setSelectedDepartment(e.target.value)}
-//               >
-//                 <option value="">Select Department</option>
-//                 {dropDownValue.data &&
-//                   dropDownValue.data.map((values, ind) => (
-//                     <option key={ind} value={values.department}>
-//                       {values.department}
-//                     </option>
-//                   ))}
-//               </select>
-//             </div>
-
-//             <div className="col-12 col-md-6">
-//               <label htmlFor="doctor" className="form-label">
-//                 Doctor Name:
-//               </label>
-//               <select
-//                 className="form-select"
-//                 id="doctor"
-//                 required
-//                 onChange={(e) => setselectedAvailibilit(e.target.value)}
-//               >
-//                 <option value="">Select Doctor</option>
-//                 {dropDownValue.data &&
-//                   dropDownValue.data
-//                     .filter(
-//                       (values) => values.department === selectedDepartment
-//                     ) // Filter doctors based on department
-//                     .flatMap((values) => values.doctors) // Extract doctors array
-//                     .map((val, ind) => (
-//                       <option key={ind} value={val.name}>
-//                         {val.name}
-//                       </option>
-//                     ))}
-//               </select>
-//             </div>
-
-//             <div className="col-12 col-md-6">
-//               <label htmlFor="dropDownValue" className="form-label">
-//                 Availibility
-//               </label>
-//               <select className="form-select" id="department" onChange={(e)=> setAvailibility(e.target.value) } required>
-//                 {dropDownValue.data &&
-//                   dropDownValue.data
-//                     .flatMap((dept) => dept.doctors) // Extract all doctors
-//                     .filter((doc) => doc.name === selectedAvailibilit) // Find selected doctor
-//                     .flatMap((doc) => doc.availability) // Extract availability
-//                     .map((day, ind) => (
-//                       <option key={ind} value={day}>
-//                         {day}
-//                       </option>
-//                     ))}
-//               </select>
-//             </div>
-//           </div>
-
-//           {/* Patient Name */}
-//           <div className="row g-3 mb-3">
-//             <div className="col-12 col-md-6">
-//               <label htmlFor="inputName6" className="form-label">
-//                 Patient's Name:
-//               </label>
-//               <input
-//                 type="text"
-//                 id="inputName6"
-//                 className="form-control"
-//                 placeholder="Enter Patient's Name"
-//                 onChange={(e)=> SetPatientName(e.target.value)}
-//                 required
-//               />
-//             </div>
-//             <div className="col-12 col-md-6">
-//               <label htmlFor="inputEmail6" className="form-label">
-//                 Patient's Email:
-//               </label>
-//               <input
-//                 type="email"
-//                 id="inputEmail6"
-//                 className="form-control"
-//                 placeholder="Enter Patient's Email"
-//                 onChange={(e)=> Setemail(e.target.value)}
-//                 required
-//               />
-//             </div>
-//           </div>
-
-//           {/* Appointment Date and Description */}
-//           <div className="row g-3 mb-3">
-//             <div className="col-12 col-md-6">
-//               <label htmlFor="appointmentDate" className="form-label">
-//                 Appointment Date:
-//               </label>
-//               <input
-//                 type="date"
-//                 id="appointmentDate"
-//                 className="form-control"
-//                 onChange={(e)=>Setdate(e.target.value) }
-//                 required
-//               />
-//             </div>
-//             <div className="col-12">
-//               <label htmlFor="descriptionInput" className="form-label">
-//                 Description:
-//               </label>
-//               <textarea
-//                 id="descriptionInput"
-//                 className="form-control"
-//                 rows="4"
-//                 placeholder="Enter description here..."
-//                 required
-//                 onChange={(e)=> Setdescryption(e.target.value)}  ></textarea>
-//             </div>
-//           </div>
-
-//           {/* Submit Button */}
-//           <div className="d-grid">
-//             <button type="submit"  className="btn btn-submit btn-primary">
-//               Submit
-//             </button>
-//           </div>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Appointment;
-
-// = await axios.get(
-//   "https://backend-node-5tca.onrender.com/api/doctor/Department",
-//   { withCredentials: true }
-// );
-// setDropDownValue(response.data); // Store only the data part
-// } catch (err) {
-// console.log("Error fetching departments:", err.message);
-// }
-// };
-
-// fetchData();
-// }, []);
